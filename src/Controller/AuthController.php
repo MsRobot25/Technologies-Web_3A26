05a8ff2f36587e4508c58b60205b376546681531
souleimaa-rebai <souleima.rebai@esprit.tn>
2025-09-25 20:26:40 +0100

    first commit for symfony

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AuthController extends AbstractController
+{
+    #[Route('/auth', name: 'app_auth')]
+    public function index(): Response
+    {
+        return $this->render('auth/index.html.twig', [
+            'controller_name' => 'AuthController',
+        ]);
+    }
+     #[Route('/auth/{name}', name: 'showAuthor')]
+     public function showAuthor($name): Response
+    {
+         return $this->render('auth/show.html.twig',
+         ['nameUser'=>$name]
+    );}
+
+    #[Route('/authors', name: 'list_authors')]
+public function listAuthors(): Response
+{
+    $authors = [
+        ['id' => 1, 'picture' => '/assets/images/default.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+        ['id' => 2, 'picture' => '/assets/images/testingshi.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+        ['id' => 3, 'picture' => '/assets/images/testlogo.png', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+    ];
+
+    return $this->render('auth/list.html.twig',
+     ['authors' => $authors,]);
+}
+#[Route('/authors/details/{id}', name: 'author_details')]
+public function authorDetails(int $id): Response
+{
+    $authors = [
+        1 => ['id' => 1, 'picture' => '/assets/images/default.jpg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+        2 => ['id' => 2, 'picture' => '/assets/images/testingshi.jpg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+        3 => ['id' => 3, 'picture' => '/assets/images/testlogo.png', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+    ];
+
+    $author = $authors[$id] ?? null;
+
+    if (!$author) {
+        throw $this->createNotFoundException("non trouvé");
+    }
+
+    return $this->render('auth/showAuthor.html.twig', [
+        'author' => $author,
+    ]);
+}
+
+
+
+}
