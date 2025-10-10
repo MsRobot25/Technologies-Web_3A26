3d11e01255b524e58c48b4bbdb043c9fbb827937
souleimaa-rebai <souleima.rebai@esprit.tn>
2025-10-10 17:07:34 +0100

    5eme seance

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Author;
+use App\Form\AuthorType;
+
 final class AuthController extends AbstractController
 {
     #[Route('/auth', name: 'app_auth')]
@@ -57,11 +63,66 @@ public function authorDetails(int $id): Response
   public function showAll(AuthorRepository $repo): Response
     {
         $authors=$repo->findAll();
-         return $this->render('auth/showAll.html.twig', 
-         parameters:['list'=>$authors]);
+         return $this->render('auth/showAll.html.twig', [
+        'list' => $authors,
+    ]);
+        
+   }
+     #[Route('/add', name: 'add')]
+  public function add(ManagerRegistry $doctrine): Response{
+$author=new Author();
+$author->setEmail (email: 'foulen@esprit.tn');
+$author->setUsername (username: 'foulen');
+$em=$doctrine->getManager();
+$em->persist(object: $author);
+$em->flush();
+return $this->redirectToRoute('showAll');
+//return new Response(content: "Author added suceesfully");
 
         
    }
+ #[Route('/delete/{id}', name: 'delete')]
+  public function deleteAuthor($id,AuthorRepository $repo ,ManagerRegistry $doctrine):Response{
+    $author=$repo->find($id);
+    $em=$doctrine->getManager();
+    $em->remove($author);
+    $em->flush();//ajout w supprission w modification bel flush
+return $this->redirectToRoute('showAll');
+
+  }
+    #[Route('/showDetails/{id}', name: 'showDetails')]
+  public function show(int $id, AuthorRepository $repo): Response
+{
+    $author = $repo->find($id);
+
+    if (!$author) {
+        throw $this->createNotFoundException('Auteur non trouvé');
+    }
+
+    return $this->render('auth/showDetails.html.twig', [
+        'author' => $author,
+    ]);
+        
+   }
+   
+    #[Route('/addForm',name:'addForm')]
+    public function addForm(Request $request, ManagerRegistry $doctrine){
+    $author=new Author();
+    $form=$this->createForm(AuthorType::class,$author);
+    $form->add('Add',SubmitType::class);
+ 
+    $form->handleRequest($request);
+    if($form->isSubmitted()){
+     $em=$doctrine->getManager();
+     $em->persist($author);
+     $em->flush();
+     return $this->redirectToRoute('showAll');
+    }
+    return $this->render('auth/add.html.twig',['formulaire'=>$form->createView()]);
+    // return $this->renderForm()
+    }
+
+
 
 
 
